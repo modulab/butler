c044cae02272fd05adf0ef418588d2a2453edb94
SeanBastable <bastabls2@gmail.com>
2013-07-12 16:20:53 +0100

    Updated brake

--- brake/brake_server.py
+++ brake/brake_server.py
@@ -11,13 +11,17 @@ client = dynamic_reconfigure.client.Client('b21')
 def brake_server():
 	rospy.init_node('brake_server')
 	rospy.Subscriber("b21/cmd_brake_power", Bool, callback)
+#	rospy.Subscriber("b21/brake_power", Bool, callback2)
 	rospy.spin()
 
 
 def callback(data):
 	params = { 'brake_enabled' : data.data }
 	config = client.update_configuration(params)
-	rospy.loginfo(rospy.get_name() + ": Changing brakes to " + str(data.data))
+#	rospy.loginfo(rospy.get_name() + ": Changing brakes to " + str(data.data))
+
+#def callback2(data):
+#	rospy.loginfo(rospy.get_name() + ": " + str(data.data))
 
 if __name__ == '__main__':
     brake_server()
--- brake/src/main/java/butler/brake/Brake.java
+++ brake/src/main/java/butler/brake/Brake.java
@@ -1,97 +1,184 @@
+package butler.brake;
+
+import geometry_msgs.Twist;
+import nav_msgs.Odometry;
+
+import org.apache.commons.logging.Log;
+import org.ros.message.MessageListener;
 import org.ros.namespace.GraphName;
-import org.ros.node.Node;
-import org.ros.node.NodeMain;
+import org.ros.node.AbstractNodeMain;
 import org.ros.node.ConnectedNode;
 import org.ros.node.topic.Publisher;
+import org.ros.node.topic.Subscriber;
+
 import sensor_msgs.JointState;
+import sensor_msgs.LaserScan;
+import sensor_msgs.PointCloud2;
+import std_msgs.Bool;
+import actionlib_msgs.GoalStatusArray;
 
-public class InitPTU implements NodeMain {
+public class Brake extends AbstractNodeMain {
 
-  @Override
-  public GraphName getDefaultNodeName() {
-    return GraphName.of("init_ptu");
-  }
+	private long lastLaser, lastPTU, lastKinect, lastMoveBase, lastOdom, lastVel, brakesLastApplied;
+	private final long LASER_FAILURE_THRESHOLD = 500000000, PTU_FAILURE_THRESHOLD = 500000000, KINECT_FAILURE_THRESHOLD = 1000000000,
+			MOVE_BASE_FAILURE_THRESHOLD = 500000000, VEL_FAILURE_THRESHOLD = 1500000000, ODOM_FAILURE_THRESHOLD = 500000000,
+			BRAKE_THRESHOLD = 5000000000l;
+	private boolean braked;
+	private Publisher<Bool> brakePub;
+	private Publisher<Twist> velPub;
+	private Odometry lastOdomMsg = null;
+	private Log log;
 
-  @Override
-  public void onStart(ConnectedNode node) {
-
-	Publisher<JointState> ptuPub = node.newPublisher("ptu/cmd", JointState._TYPE);
-
-	try {
-		Thread.sleep(13000);
-	}
-	catch (InterruptedException e) {
-		e.printStackTrace();
-	}
-
-	JointState ptuCmd = ptuPub.newMessage();
-	ptuCmd.getName().add("head_pan_joint");
-	ptuCmd.getName().add("head_tilt_joint");
-
-/*	ptuCmd.setPosition(new double[] {0,-0.8});
-	ptuCmd.setVelocity(new double[] {1,1});
-	ptuPub.publish(ptuCmd);
-	try {
-		Thread.sleep(3000);
-	}
-	catch (InterruptedException e) {
-		e.printStackTrace();
+	@Override
+	public GraphName getDefaultNodeName() {
+		return GraphName.of("brake");
 	}
 
-	ptuCmd.setPosition(new double[] {0,0.5});
-	ptuPub.publish(ptuCmd);
+	@Override
+	public void onStart(ConnectedNode node) {
+		log = node.getLog();
 
-	try {
-		Thread.sleep(3000);
+		brakePub = node.newPublisher("b21/cmd_brake_power", Bool._TYPE);
+
+		velPub = node.newPublisher("b21/cmd_vel", Twist._TYPE);
+
+		Subscriber<LaserScan> laserSub = node.newSubscriber("scan", LaserScan._TYPE);
+
+		laserSub.addMessageListener(new MessageListener<LaserScan>() {
+			@Override
+			public void onNewMessage(LaserScan update) {
+				lastLaser = System.nanoTime();
+			}
+		});
+
+		Subscriber<JointState> ptuSub = node.newSubscriber("ptu/state", JointState._TYPE);
+
+		ptuSub.addMessageListener(new MessageListener<JointState>() {
+			@Override
+			public void onNewMessage(JointState update) {
+				lastPTU = System.nanoTime();
+			}
+		});
+
+		Subscriber<PointCloud2> kinectSub = node.newSubscriber("voxel_grid/output", PointCloud2._TYPE);
+
+		kinectSub.addMessageListener(new MessageListener<PointCloud2>() {
+			@Override
+			public void onNewMessage(PointCloud2 update) {
+				lastKinect = System.nanoTime();
+			}
+		});
+
+		Subscriber<GoalStatusArray> moveBaseSub = node.newSubscriber("move_base/status", GoalStatusArray._TYPE);
+
+		moveBaseSub.addMessageListener(new MessageListener<GoalStatusArray>() {
+			@Override
+			public void onNewMessage(GoalStatusArray update) {
+				lastMoveBase = System.nanoTime();
+			}
+		});
+
+		Subscriber<Odometry> odomSub = node.newSubscriber("b21/odom", Odometry._TYPE);
+
+		odomSub.addMessageListener(new MessageListener<Odometry>() {
+			@Override
+			public void onNewMessage(Odometry update) {
+				lastOdom = System.nanoTime();
+				lastOdomMsg = update;
+			}
+		});
+
+		Subscriber<Twist> velSub = node.newSubscriber("b21/cmd_vel", Twist._TYPE);
+
+		velSub.addMessageListener(new MessageListener<Twist>() {
+			@Override
+			public void onNewMessage(Twist update) {
+				lastVel = System.nanoTime();
+			}
+		});
+
+		Subscriber<Bool> brakeSub = node.newSubscriber("b21/brake_power", Bool._TYPE);
+
+		brakeSub.addMessageListener(new MessageListener<Bool>() {
+			@Override
+			public void onNewMessage(Bool update) {
+				// if (!update.getData())
+				// braked = update.getData();
+				// System.out.println(braked);
+			}
+		});
+
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+
+		while (true) {
+			if (System.nanoTime() - brakesLastApplied > BRAKE_THRESHOLD) {
+				if (System.nanoTime() - lastLaser > LASER_FAILURE_THRESHOLD) {
+
+					System.out.println("Laser messages not received for over 500ms. Braking.");
+					brake();
+				}
+
+				else if (System.nanoTime() - lastPTU > PTU_FAILURE_THRESHOLD) {
+
+					System.out.println("PTU messages not received for over 500ms. Braking.");
+					brake();
+				}
+
+				else if (System.nanoTime() - lastKinect > KINECT_FAILURE_THRESHOLD) {
+
+					System.out.println("Kinect messages not received for over 1000ms. Braking.");
+					brake();
+				}
+
+				else if (System.nanoTime() - lastMoveBase > MOVE_BASE_FAILURE_THRESHOLD) {
+
+					System.out.println("Move_base messages not received for over 500ms. Braking.");
+					brake();
+					velPub.publish(velPub.newMessage());
+				}
+
+				else if (System.nanoTime() - lastVel > VEL_FAILURE_THRESHOLD) {
+					if (lastOdomMsg == null) {
+						System.out.println("No odometry messages received. Braking.");
+						brake();
+					}
+
+					// else if ((System.nanoTime() - lastOdom <
+					// ODOM_FAILURE_THRESHOLD)
+					// && (lastOdomMsg.getTwist().getTwist().getLinear().getX()
+					// > 0.04
+					// || lastOdomMsg.getTwist().getTwist().getLinear().getX() <
+					// -0.04
+					// || lastOdomMsg.getTwist().getTwist().getLinear().getY() >
+					// 0.04
+					// || lastOdomMsg.getTwist().getTwist().getLinear().getY() <
+					// -0.04
+					// || lastOdomMsg.getTwist().getTwist().getAngular().getZ()
+					// > 0.09 || lastOdomMsg.getTwist().getTwist()
+					// .getAngular().getZ() < -0.09)) {
+					// System.out.println("No velocity messages received for over 500ms but the robot is still moving. Braking.");
+					// brake();
+					// velPub.publish(velPub.newMessage());
+					// }
+				}
+			}
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-	catch (InterruptedException e) {
-		e.printStackTrace();
+
+	private void brake() {
+		Bool newBrakeMsg = brakePub.newMessage();
+		newBrakeMsg.setData(true);
+		brakePub.publish(newBrakeMsg);
+		brakesLastApplied = System.nanoTime();
+		braked = true;
 	}
-
-	ptuCmd.setPosition(new double[] {2.5,0});
-	ptuPub.publish(ptuCmd);
-	try {
-		Thread.sleep(3000);
-	}
-	catch (InterruptedException e) {
-		e.printStackTrace();
-	}
-
-	ptuCmd.setPosition(new double[] {-2.5,0});
-	ptuPub.publish(ptuCmd);
-
-	try {
-		Thread.sleep(3000);
-	}
-	catch (InterruptedException e) {
-		e.printStackTrace();
-	} */
-
-	ptuCmd.setPosition(new double[] {0,-0.81});
-	ptuCmd.setVelocity(new double[] {1,1});
-	ptuPub.publish(ptuCmd);
-
-	try {
-		Thread.sleep(1000);
-	}
-	catch (InterruptedException e) {
-		e.printStackTrace();
-	}
-
-	ptuCmd.setPosition(new double[] {0,-0.82});
-	ptuCmd.setVelocity(new double[] {1,1});
-	ptuPub.publish(ptuCmd);
-  }
-
-  @Override
-  public void onShutdown(Node node) {
-  }
-
-  @Override
-  public void onShutdownComplete(Node node) {
-  }
-
-  @Override
-  public void onError(Node node, Throwable throwable) {
-  }
 }
