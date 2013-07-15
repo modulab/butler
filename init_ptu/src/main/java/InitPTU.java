74d0667f5922ffbeb13a33cbb9dc401c2a0d5484
SeanBastable <bastabls2@gmail.com>
2013-07-15 16:44:06 +0100

    init_ptu cleanup

--- init_ptu/src/main/java/InitPTU.java
+++ init_ptu/src/main/java/InitPTU.java
@@ -1,97 +1,43 @@
 import org.ros.namespace.GraphName;
-import org.ros.node.Node;
-import org.ros.node.NodeMain;
+import org.ros.node.AbstractNodeMain;
 import org.ros.node.ConnectedNode;
 import org.ros.node.topic.Publisher;
+
 import sensor_msgs.JointState;
 
-public class InitPTU implements NodeMain {
+public class InitPTU extends AbstractNodeMain {
 
-  @Override
-  public GraphName getDefaultNodeName() {
-    return GraphName.of("init_ptu");
-  }
-
-  @Override
-  public void onStart(ConnectedNode node) {
-
-	Publisher<JointState> ptuPub = node.newPublisher("ptu/cmd", JointState._TYPE);
-
-	try {
-		Thread.sleep(13000);
-	}
-	catch (InterruptedException e) {
-		e.printStackTrace();
+	@Override
+	public GraphName getDefaultNodeName() {
+		return GraphName.of("butler/init_ptu");
 	}
 
-	JointState ptuCmd = ptuPub.newMessage();
-	ptuCmd.getName().add("head_pan_joint");
-	ptuCmd.getName().add("head_tilt_joint");
+	@Override
+	public void onStart(ConnectedNode node) {
 
-/*	ptuCmd.setPosition(new double[] {0,-0.8});
-	ptuCmd.setVelocity(new double[] {1,1});
-	ptuPub.publish(ptuCmd);
-	try {
-		Thread.sleep(3000);
+		Publisher<JointState> ptuPub = node.newPublisher("ptu/cmd", JointState._TYPE);
+
+		try {
+			Thread.sleep(13000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		JointState ptuCmd = ptuPub.newMessage();
+		ptuCmd.getName().add("head_pan_joint");
+		ptuCmd.getName().add("head_tilt_joint");
+		ptuCmd.setPosition(new double[] { 0, -0.81 });
+		ptuCmd.setVelocity(new double[] { 1, 1 });
+		ptuPub.publish(ptuCmd);
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		ptuCmd.setPosition(new double[] { 0, -0.82 });
+		ptuCmd.setVelocity(new double[] { 1, 1 });
+		ptuPub.publish(ptuCmd);
 	}
-	catch (InterruptedException e) {
-		e.printStackTrace();
-	}
-
-	ptuCmd.setPosition(new double[] {0,0.5});
-	ptuPub.publish(ptuCmd);
-
-	try {
-		Thread.sleep(3000);
-	}
-	catch (InterruptedException e) {
-		e.printStackTrace();
-	}
-
-	ptuCmd.setPosition(new double[] {2.5,0});
-	ptuPub.publish(ptuCmd);
-	try {
-		Thread.sleep(3000);
-	}
-	catch (InterruptedException e) {
-		e.printStackTrace();
-	}
-
-	ptuCmd.setPosition(new double[] {-2.5,0});
-	ptuPub.publish(ptuCmd);
-
-	try {
-		Thread.sleep(3000);
-	}
-	catch (InterruptedException e) {
-		e.printStackTrace();
-	} */
-
-	ptuCmd.setPosition(new double[] {0,-0.81});
-	ptuCmd.setVelocity(new double[] {1,1});
-	ptuPub.publish(ptuCmd);
-
-	try {
-		Thread.sleep(1000);
-	}
-	catch (InterruptedException e) {
-		e.printStackTrace();
-	}
-
-	ptuCmd.setPosition(new double[] {0,-0.82});
-	ptuCmd.setVelocity(new double[] {1,1});
-	ptuPub.publish(ptuCmd);
-  }
-
-  @Override
-  public void onShutdown(Node node) {
-  }
-
-  @Override
-  public void onShutdownComplete(Node node) {
-  }
-
-  @Override
-  public void onError(Node node, Throwable throwable) {
-  }
 }
