907f6c130565bf301913e55c606a3b9f904f9a85
SeanBastable <bastabls2@gmail.com>
2013-08-09 09:49:11 +0100

    Cleanup

--- brake/brake_server.py
+++ brake/brake_server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-PACKAGE = 'butler'
+PACKAGE = 'brake'
 import roslib;roslib.load_manifest(PACKAGE)
 import rospy
 import dynamic_reconfigure.client
@@ -11,17 +11,12 @@ client = dynamic_reconfigure.client.Client('b21')
 def brake_server():
 	rospy.init_node('brake_server')
 	rospy.Subscriber("b21/cmd_brake_power", Bool, callback)
-#	rospy.Subscriber("b21/brake_power", Bool, callback2)
 	rospy.spin()
 
 
 def callback(data):
 	params = { 'brake_enabled' : data.data }
 	config = client.update_configuration(params)
-#	rospy.loginfo(rospy.get_name() + ": Changing brakes to " + str(data.data))
-
-#def callback2(data):
-#	rospy.loginfo(rospy.get_name() + ": " + str(data.data))
 
 if __name__ == '__main__':
     brake_server()
