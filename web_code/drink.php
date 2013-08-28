c207a05eb14c7c23be4f9b6948fa13b2ae838596
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2013-08-28 13:36:21 +0100

    Adding Twitter Bootstrap CSS.

--- web_code/drink.php
+++ web_code/drink.php
@@ -1,29 +1,114 @@
-<html>
-<head>
-<title>Order a Drink</title>
-</head>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>ADAM - Autonomous Drinks Administering Machine</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Autonomous Drinks Administering Machine">
+    <meta name="author" content="School of Computer Science, University of Birmingham">
+
+    <!-- Le styles -->
+    <link href="./assets/css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 20px;
+        padding-bottom: 40px;
+      }
+
+      /* Custom container */
+      .container-narrow {
+        margin: 0 auto;
+        max-width: 700px;
+      }
+      .container-narrow > hr {
+        margin: 30px 0;
+      }
+
+      /* Main marketing message and sign up button */
+      .jumbotron {
+        margin: 60px 0;
+        text-align: center;
+      }
+      .jumbotron h1 {
+        font-size: 72px;
+        line-height: 1;
+      }
+      .jumbotron .btn {
+        font-size: 21px;
+        padding: 14px 24px;
+      }
+
+      /* Supporting marketing content */
+      .marketing {
+        margin: 60px 0;
+      }
+      .marketing p + h4 {
+        margin-top: 28px;
+      }
+    </style>
+    <link href="./assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="./assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="./assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="./assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="./assets/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="./assets/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="./assets/ico/favicon.png">
+  </head>
 <body>
-<?PHP 
+<?PHP  
 $station=$_GET["station"];
 ?>
+<div class="container-narrow">
+	<div class="masthead">
+		<h3 class="muted"><?php 
+			echo "Welcome to <span class=\"text-error\">ADAM</span> Station $station";
+		?></h3>
+	</div>
 
-<h1><?php 
-echo "Welcome to Station $station";
-?></h1>
-<p> Please place your order: </p>
+
+
 <form action="order.php" method="post">
+	<fieldset>
+		<legend>Please place your order:</legend>
 <?php
 echo "<input type='hidden' name='station_id' value=$station>";
 ?>
-Your name: <input type="text" name="name"><br>
-How many beers? I can't carry more than 3..
+<label>Your name: (optional)</label>
+<input type="text" name="name">
+<label>How many beers? I can't carry more than 3...</label>
+
 <select name="drinks">
 <option value="1beer"selected>1</option>
 <option value="2beer">2</option>
 <option value="3beer" >3</option>
 </select><br>
 
-<input type="submit">
+<button type="submit" class="btn btn-primary">Order</button>
+</fieldset>
 </form>
+</div>
+ <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./assets/js/jquery.js"></script>
+    <script src="./assets/js/bootstrap-transition.js"></script>
+    <script src="./assets/js/bootstrap-alert.js"></script>
+    <script src="./assets/js/bootstrap-modal.js"></script>
+    <script src="./assets/js/bootstrap-dropdown.js"></script>
+    <script src="./assets/js/bootstrap-scrollspy.js"></script>
+    <script src="./assets/js/bootstrap-tab.js"></script>
+    <script src="./assets/js/bootstrap-tooltip.js"></script>
+    <script src="./assets/js/bootstrap-popover.js"></script>
+    <script src="./assets/js/bootstrap-button.js"></script>
+    <script src="./assets/js/bootstrap-collapse.js"></script>
+    <script src="./assets/js/bootstrap-carousel.js"></script>
+    <script src="./assets/js/bootstrap-typeahead.js"></script>
+
 </body>
 </html>
