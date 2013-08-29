78281d9a105bb46d97b41f83c9df017e213424e7
cburbridge <cburbridge@gmail.com>
2013-08-29 21:43:04 +0100

    service manager gui to see and cancel orders

---
+++ service_manager/src/manager.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('service_manager')
+import rospy
+#rospy.load_manifest("service_manager")
+from web_connector.srv import *
+
+import pygtk
+import gtk
+import gobject
+
+class App(object):
+    def __init__(self):
+        self.main_window = gtk.Window()
+        self.main_window.set_title("Service Manager")
+        self.main_window.connect('destroy', gtk.main_quit)
+        self.main_window.set_size_request(400, 200)
+        vbox = gtk.VBox(False, 8)
+
+        sw = gtk.ScrolledWindow()
+        sw.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+        sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        
+        vbox.pack_start(sw, True, True, 0)
+        self.button_box = gtk.HBox()
+
+        vbox.pack_start(self.button_box, False, False)
+
+        refresh_btn = gtk.Button("Refresh")
+        refresh_btn.connect("clicked",self.refresh_btn_cb)
+        self.button_box.pack_start(refresh_btn)
+
+        completed_btn = gtk.Button("Mark Complete")
+        completed_btn.connect("clicked",self.completed_btn_cb)
+        self.button_box.pack_start(completed_btn)
+
+
+        self.orders_list_store = gtk.ListStore(str, str, str, str)
+        self.orders_list_store.append(["0","0","0","0"])
+        self.orders_view = gtk.TreeView(self.orders_list_store)
+        self.orders_view.get_selection().set_mode(gtk.SELECTION_MULTIPLE)
+
+
+        for num,i in enumerate(["Order Numer", "Station", "Order", "Customer Name"]):
+            renderText = gtk.CellRendererText()
+            column = gtk.TreeViewColumn(i, renderText, text=num)
+            self.orders_view.append_column(column)
+ 
+        sw.add(self.orders_view)
+
+        self.main_window.add(vbox)
+
+        self.node = rospy.init_node("service_manager")
+ 
+        
+    def run(self):
+        self.main_window.show_all()
+
+    def get_selected(self):
+        (model, pathlist) = self.orders_view.get_selection().get_selected_rows()
+        selected = []
+        for i in pathlist:
+            print self.orders[i[0]]
+            selected.append( self.orders[i[0]] )
+        return selected
+            
+
+    def refresh_orders(self):
+        rospy.wait_for_service("get_orders")
+        get_orders = rospy.ServiceProxy("get_orders", GetOrders)
+        resp = get_orders()
+        self.orders = resp.orders
+        self.orders_list_store.clear()
+        for i in self.orders:
+            self.orders_list_store.append([i.order_id, i.station_id, i.drinks, i.name])
+
+    def refresh_btn_cb(self, btn):
+        self.refresh_orders()
+
+    def completed_btn_cb(self,btn):
+        selected = self.get_selected()
+        rospy.wait_for_service("get_orders")
+        mark_order_complete = rospy.ServiceProxy("mark_order_complete", MarkOrderComplete)
+        for i in selected:
+            resp = mark_order_complete(i.order_id)
+        self.refresh_orders()
+
+        
+
+if __name__ == '__main__':
+    m = App()
+    m.run()
+
+    gtk.main()
+
