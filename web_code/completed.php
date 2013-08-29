05fafc197ab99bcccd21749959200b48c2679596
cburbridge <cburbridge@gmail.com>
2013-08-29 17:39:23 +0100

    fix bug in order completion

--- web_code/completed.php
+++ web_code/completed.php
@@ -13,28 +13,22 @@ if (flock($lock, LOCK_EX)) {
 	/////////////////////////////////////
 	// open the orders file and remove the line 
 
+	// read the order lines
+	$lines = file('data/orders.txt');
+
 	// write back to the file
 	if (!$file = fopen("data/orders.txt", 'w')) { 
 		print "Oh No! Err Code=1"; 
 		exit; 
 	} 
 
-	if (flock($file,LOCK_EX)) {
-
-		// read the order lines
-		$lines = file('data/orders.txt');
-
-
-		foreach ($lines as $line) {
-		  $entry = explode(" ", $line,2);
-		  if ($entry[0] != $order_no) {
-		   fwrite($file, $line);
-		  }
-		}
-	} else {
-		echo "Error 42.";
+	foreach ($lines as $line) {
+	  $entry = explode(" ", $line,2);
+	  if ($entry[0] != $order_no) {
+	   fwrite($file, $line);
+	  }
 	}
-
+	
 	fclose($file);
 	////////////////////////////////////
 
@@ -45,13 +39,10 @@ if (flock($lock, LOCK_EX)) {
 		print "Oh No! Err Code=1"; 
 		exit; 
 	}
-	if (flock($file, LOCK_EX)) { 
-		fwrite($file, time()."\n" );
-	} else {
-		echo "Error 42.1";
-	}
+	fwrite($file, time()."\n" );	
 	fclose($file);
 	///////////////////////////////////
+
 	flock($lock, LOCK_UN);	
 } else {
 	echo "Error 42.";
