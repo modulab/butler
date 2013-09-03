8f74acaba8fca51169dc8dea9ea88c145e10be2a
cburbridge <cburbridge@gmail.com>
2013-09-03 09:32:20 +0100

    GetMarkOrders parameterised max drinks ready for use after stolen drinks

--- butler_executive/src/b21.py
+++ butler_executive/src/b21.py
@@ -24,10 +24,12 @@ The selected orders are then stored in the globabl application data singleton
 for use in other states.
 """
 class GetAndMarkOrders(smach.State):
-    def __init__(self):
+    def __init__(self, max_beers=6):
         smach.State.__init__(self,
             outcomes    = ['succeeded', 'no_orders']
         )
+        self.max_beers = max_beers
+        
         # create client for service
         try:
             rospy.wait_for_service("get_orders", 4)
@@ -62,7 +64,7 @@ class GetAndMarkOrders(smach.State):
             if order.station_id != service_station:
                 continue
             carrying += order_weights[order.drinks]
-            if carrying > MAX_LOAD:
+            if carrying > self.max_beers:
                 carrying -= order_weights[order.drinks]
                 break
             this_round.append(order)
