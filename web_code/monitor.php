dab6ed71c9fbb9581bb4a8d6ae24024dd3179486
cburbridge <cburbridge@gmail.com>
2013-09-04 14:06:39 +0100

    generalised monitor.php for reuse in display

--- web_code/monitor.php
+++ web_code/monitor.php
@@ -63,130 +63,146 @@
   </head>
 <body>
 	<div class="container-narrow">
-<h1>I'm working on it!</h1>
 <?PHP 
-$lock = fopen("data/lock","w+");
+   $lock = fopen("data/lock","w+");
 if (flock($lock, LOCK_EX)) {
+  if ($_GET["id"]=="") {
+	$order_no=-1;
+  } else {
 	$order_no=$_GET["id"];
+  }
 
-	//////////////////////////////
-	// Read the orders
-	$orders = file("data/orders.txt");
-	$position=0;
-	$set=0;
-	foreach ($orders as $order) {
-		$position = $position +1;
-		$entry = explode(" ", $order,2);
-		if ($entry[0] == $order_no) {
-		  $set=1;
-		  break;
-		}
-	}
-	if ($set==0) {
-		echo "ERROR - Order number does not exist!";
-		exit;
-	}
-	//////////////////////////////
 
-	//////////////////////////////
-	// approximate time per order
-	$times=file("data/closures.txt");
-	$number=0;
-	$total=0;
-	foreach ($times as $time) {
-		$total = intval($time) - intval($times[0]);
-		$number = $number + 1;
+  //////////////////////////////
+  // Read the orders
+  $orders = file("data/orders.txt");
+  $position=0;
+  $set=0;
+  foreach ($orders as $order) {
+	$position = $position +1;
+	$entry = explode(" ", $order,2);
+	if ($entry[0] == $order_no) {
+	  $set=1;
+	  break;
 	}
-	$average = $total / $number;
-	//////////////////////////////
-
-	/////////////////////////////
-	// find out what orders are active
-	$actives = file("data/active.txt");
-	$is_active=0;
-	foreach ($actives as $active) {
-		if (intval($active) == intval($order_no)) {
-		  $is_active = 1;
-		  break;
-		}
+  }
+  if (($set==0) && ($order_no!=-1)) {
+	echo "ERROR - Order number does not exist!";
+	exit;
+  }
+  //////////////////////////////
+  /*
+//////////////////////////////
+// approximate time per order
+$times=file("data/closures.txt");
+$number=0;
+$total=0;
+foreach ($times as $time) {
+$total = intval($time) - intval($times[0]);
+$number = $number + 1;
+}
+$average = $total / $number;
+//////////////////////////////
+*/
+  /////////////////////////////
+  // find out what orders are active
+  $actives = file("data/active.txt");
+  $is_active=0;
+  foreach ($actives as $active) {
+	if (intval($active) == intval($order_no)) {
+	  $is_active = 1;
+	  break;
 	}
-	/////////////////////////////
-
+  }
+  /////////////////////////////
+  /*
 	$eta = $position * $average;
+  */
 
+  if ($order_no!=-1) {
+	echo "<h1>I'm working on it!</h1>";
+  
 	echo "<h3> Your are <b class=\"text-info\">position $position</b> in the queue</h3>";
 	if ($is_active==1) {
-		echo "<h4>I've picked your order up, and am on my way :-)</h4>";
+	  echo "<h4>I've picked your order up, and am on my way :-)</h4>";
 	}
-    echo "<p><table class=\"table table-condensed\">";
-    echo "<tr><th>Status</th><th>Name</th><th>No. Beers</th><th>Drink Station</th><th>Order #</th></tr>";
-    foreach ($orders as $order) {
-        $is_active=0;
+  }
+  echo "<p><table class=\"table table-condensed\">";
+  echo "<tr><th>Status</th><th>Name</th><th>No. Beers</th><th>Drink Station</th><th>Order #</th></tr>";
+  foreach ($orders as $order) {
+	$is_active=0;
 
 
-       $entry = explode(" ", $order,4);
-    	foreach ($actives as $active) {
-	    	if (intval($active) == intval($entry[0])) {
-		      $is_active = 1;
-		      break;
-	    	}
-	    }
+	$entry = explode(" ", $order,4);
+	foreach ($actives as $active) {
+	  if (intval($active) == intval($entry[0])) {
+		$is_active = 1;
+		break;
+	  }
+	}
 
-       if ($entry[0] == $order_no) {
-          echo "<tr class=\"error\"> ";
-	   } else if ($is_active==1) {
-           echo "<tr class=\"warning\">";
-       } else {
-           echo "<tr>";
-       }
-       echo "<td>";
-       if ($is_active==1){
-           echo "<span class=\"badge badge-info\">On Route</span>";
-       }
-       echo "</td>";
-       echo "<td>";
-       echo $entry[3];
-       echo "</td>";
+	if ($entry[0] == $order_no) {
+	  echo "<tr class=\"error\"> ";
+	} else if ($is_active==1) {
+	  echo "<tr class=\"warning\">";
+	} else {
+	  echo "<tr>";
+	}
+	echo "<td>";
+	if ($is_active==1){
+	  echo "<span class=\"badge badge-info\">On Route</span>";
+	}
+	echo "</td>";
+	echo "<td>";
+	echo $entry[3];
+	echo "</td>";
 
-       echo "<td>";
-       switch ($entry[2]){
-           case "1beer":
-                echo "1"; break;
-           case "2beer":
-                echo "2"; break;
-           case "3beer":
-                echo "3"; break;
-       }
-       echo "</td>";
+	echo "<td>";
+	switch ($entry[2]){
+	case "1beer":
+	  echo "1"; break;
+	case "2beer":
+	  echo "2"; break;
+	case "3beer":
+	  echo "3"; break;
+	}
+	echo "</td>";
 
-       echo "<td>";
-       echo $entry[1];
-       echo "</td>";
+	echo "<td>";
+	echo $entry[1];
+	echo "</td>";
 
-       echo "<td>";
-       echo $entry[0];
-       echo "</td>";
-       echo "</tr>";
-    }
-    echo "</table>";
-	flock($lock, LOCK_UN);	
+	echo "<td>";
+	echo $entry[0];
+	echo "</td>";
+	echo "</tr>";
+  }
+  echo "</table>";
+  flock($lock, LOCK_UN);	
 } else {
-	echo "Error 42.";	
+  echo "Error 42.";	
 }
 fclose($lock);
 
 ?>
+<?php
+  if ($order_no!=-1) {
+	echo "
 <hr/>
-<p class="masthead">
+<p class=\"masthead\">
     
-      <span class="text-error">ADAM</span> - the 
-      <span class="muted">Autonomous Drinks Administering Machine</span> <small>from <a href="http://www.cs.bham.ac.uk/go/irlab">the Intelligent Robotics Lab</a> in the <a href="http://www.cs.bham.ac.uk/">School of Computer Science</a>, University of Birmingham.</small>
+      <span class=\"text-error\">ADAM</span> - the 
+      <span class=\"muted\">Autonomous Drinks Administering Machine</span> <small>from <a href=\"http://www.cs.bham.ac.uk/go/irlab\">the Intelligent Robotics Lab</a> in the <a href=\"http://www.cs.bham.ac.uk/\">School of Computer Science</a>, University of Birmingham.</small>
     
   </p>
 <p >
-<small>  <a href="about.php">More information</a>
+<small>  <a href=\"about.php\">More information</a>
 </p>
 </div>
+
+    ";
+  }
+?>
  <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
