8489d9f59aa07d914d18d1d2097d9ba205fdb07f
cburbridge <cburbridge@gmail.com>
2013-09-09 19:27:45 +0100

    adding points to fakery

--- butler/src/fake_points.py
+++ butler/src/fake_points.py
@@ -7,8 +7,18 @@ from geometry_msgs.msg import Point32
 
 
 # The points as a list of (x,y,z)
-pts = [(5,4,3),
-       (7,4,3),
+pts = [ (29.2, -49.6, 1),
+        (28.5, -49,  1),
+        (28.1, -50.8, 1.0),
+        (27.7, -51.5, 1.0),
+        (26.7, -50.6, 1.0),
+        (26.0, -50.0, 1.0),
+
+        (31.9, -46.4, 1.0),
+        (32.6, -45.0, 1.0),
+        (33.7, -43.9, 1.0),
+        (33.1, -43.5, 1.0)
+       
        ]
 
 if __name__ == '__main__':
@@ -19,6 +29,7 @@ if __name__ == '__main__':
     points.header.frame_id="/map"
     for i in pts:
         p = Point32()
+        print i
         p.x, p.y, p.z = i
         points.points.append(p)
     
